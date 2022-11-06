283ca7017538f725425747ea1222d77646dd008e
Brian Mason <brian.kw.mason@gmail.com>
2022-11-06 09:55:20 -0500

    Home Page v1 Complete

---
+++ src/HomePage/Home.page.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+import pink from "../assets/pink.jpg";
+import white from "../assets/white.jpeg";
+import blue from "../assets/blue.png";
+import yellow from "../assets/yellow.jpg";
+
+export const Home = () => {
+  return (
+    <div className={styles.homePageContainer}>
+      <div className={styles.left}>
+        <h2 className={styles.satisfaction}>100% SATISFACTION GUARANTEE</h2>
+        <h1 className={styles.title}>
+          Get Quality <span>Instruction</span> Through 1-on-1 {""}
+          <span>Peer Tutoring</span>
+        </h1>
+        <p className={styles.description}>
+          Private 1-on-1 lessons with a vetted high school student in any
+          subject or sport
+        </p>
+        <div className={styles.buttonContainer}>
+          <a href="#">
+            <button className={styles.button}>Schedule a Session</button>
+          </a>
+          <a href="#">
+            <button className={`${styles.button} ${styles.blue}`}>
+              Join our Team
+            </button>
+          </a>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={`${styles.dot} ${styles.blueDot}`}></div>
+        <div className={`${styles.dot} ${styles.yellowDot}`}></div>
+        <div className={`${styles.dot} ${styles.smallPinkLeft}`}></div>
+        <div className={`${styles.dot} ${styles.smallPinkRight}`}></div>
+        <img
+          className={styles.image}
+          src={pink}
+          alt="Girl graduating"
+          id={styles.firstImage}
+        />
+        <img className={styles.image} src={white} alt="Boy graduating" />
+        <img className={styles.image} src={blue} alt="Boy graduating" />
+        <img
+          className={styles.image}
+          src={yellow}
+          alt="Girl graduating"
+          id={styles.lastImage}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Navbar/Navbar.components.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import logo from "../../assets/logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Navbar = () => {
+  const [showItems, setShowItems] = useState(false);
+
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
+          <a href="#">
+            <img src={logo} alt="CodeUp Logo" className={styles.logo} />
+          </a>
+        </div>
+        <div className={styles.linksContainer}>
+          <div className={styles.links}>
+            <a href="#" className={styles.link}>
+              Home
+            </a>
+            <a href="#" className={styles.link}>
+              About Us
+            </a>
+            <a href="#" className={styles.link}>
+              Contact Us
+            </a>
+          </div>
+
+          <div className={styles.buttonContainer}>
+            <a href="#" target="_blank" rel="noreferrer">
+              <button className={styles.button}>Schedule a session</button>
+            </a>
+          </div>
+        </div>
+
+        <GiHamburgerMenu
+          className={styles.burger}
+          onClick={() => setShowItems(!showItems)}
+        />
+      </div>
+      {showItems && (
+        <div className={styles.mobileLinksContainer}>
+          <a
+            href="#"
+            className={styles.mobileLink}
+            onClick={() => setShowItems(false)}
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className={styles.mobileLink}
+            onClick={() => setShowItems(false)}
+          >
+            About Us
+          </a>
+          <a href="#" className={styles.mobileLink}>
+            Contact Us
+          </a>
+          <a href="#" target="_blank" rel="noreferrer">
+            <button
+              className={styles.mobileButton}
+              onClick={() => setShowItems(false)}
+            >
+              Schedule a session
+            </button>
+          </a>
+          <hr className={styles.horizontal} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
